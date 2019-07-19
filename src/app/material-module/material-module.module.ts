b2f3a7ee39b6161bcce9f75c634623fc6e212c4d
nicole <nikybg96@gmail.com>
2019-07-19 00:15:54 -0400

    interface login

--- src/app/material-module/material-module.module.ts
+++ src/app/material-module/material-module.module.ts
@@ -1,31 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-//Material
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
+// Material
 import { MatCardModule } from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
     MatButtonModule,
-    MatCheckboxModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatMenuModule,
-    MatCardModule
+    MatIconModule
   ],
   exports: [
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
     MatButtonModule,
-    MatCheckboxModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatMenuModule,
-    MatCardModule
+    MatIconModule
   ]
 })
 export class MaterialModule { }
