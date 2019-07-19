2321f6476eb0c598728f7ea83c6c0a383c5911a2
nicole <nikybg96@gmail.com>
2019-07-19 14:21:37 -0400

    user register, form user

--- src/app/user/users-form/users-form.component.ts
+++ src/app/user/users-form/users-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+// Model
+import { UserModel } from '../../shared/models/user.model';
+
+// Service
+import { UserService } from '../../shared/services/user.service';
+import { User } from 'firebase';
 
 @Component({
   selector: 'app-users-form',
@@ -7,9 +15,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersFormComponent implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  @Input() user: UserModel;
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onSubmit: EventEmitter<any>;
+
+  constructor(private userService: UserService, private formBuilder: FormBuilder) {
+    this.onSubmit = new EventEmitter <any>();
+  }
 
   ngOnInit() {
+    this.user = new UserModel();
+  }
+
+  save() {
+    this.onSubmit.emit(this.user);
   }
 
 }
