d478b72b2649ee6fcf421049445209b489a23f56
nicole <nikybg96@gmail.com>
2019-07-19 12:21:21 -0400

    register user

--- src/app/user/register-user/register-user.component.ts
+++ src/app/user/register-user/register-user.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+// Model
+import { UserModel } from '../../shared/models/user.model';
+
+// Services
+import { UserService } from '../../shared/services/user.service';
+import { AuthentificationService } from '../../authentification/authentification.service';
 
 @Component({
   selector: 'app-register-user',
@@ -7,9 +15,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthentificationService, private userService: UserService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  register(user: UserModel) {
+    this.userService.createUser(user);
+    this.authService.register(user);
+    this.router.navigate(['auth/login']);
   }
 
 }
