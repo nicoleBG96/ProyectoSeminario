429cf62fbaa787dea5772c02f61eb60dd42aaf01
nicole <nikybg96@gmail.com>
2019-08-05 20:39:28 -0400

    fix child routing

--- src/app/child/child-routing.module.ts
+++ src/app/child/child-routing.module.ts
@@ -3,10 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 // Components
 import { ChildRegisterComponent } from './register/child-register/child-register.component';
+import { EditRegisterChildComponent } from './register/edit-register-child/edit-register-child.component';
+import { RegisterChildMedicalRecordComponent } from './medicalRecord/register-child-medical-record/register-child-medical-record.component';
 
 
 const routes: Routes = [
-  {path: 'registerChild', component: ChildRegisterComponent}
+  {path: 'registerChild', component: ChildRegisterComponent},
+  {path: 'editRegisterChild', component: EditRegisterChildComponent},
+  {path: 'registerChildMedicalRecord', component: RegisterChildMedicalRecordComponent}
 ];
 
 @NgModule({
