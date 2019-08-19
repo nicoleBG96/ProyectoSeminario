caece7848bcaa02e895702363113790ea6dffb0e
nicole <nikybg96@gmail.com>
2019-08-19 19:18:12 -0400

    donations model

---
+++ src/app/shared/models/donations.model.ts
@@ -0,0 +1,11 @@
+export class Donations {
+    date: Date;
+    amount: string;
+    description: string;
+
+    constructor() {
+        this.date = null;
+        this.amount = '';
+        this.description = '';
+    }
+}
