657fe9005ba70f3ff2c9994519e831cc5f6043f6
nicole <nikybg96@gmail.com>
2019-08-19 22:21:05 -0400

    finance routing

--- src/app/finance/finance-routing.module.ts
+++ src/app/finance/finance-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Components
+import { RegisterDonationComponent } from './donations/register-donation/register-donation.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'registerDonation', component: RegisterDonationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
