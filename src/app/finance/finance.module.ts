a3ccccf7cca659931342a7206dd0f75b2cbd0034
nicole <nikybg96@gmail.com>
2019-08-23 22:02:40 -0400

    donation form

--- src/app/finance/finance.module.ts
+++ src/app/finance/finance.module.ts
@@ -5,11 +5,13 @@ import { SharedModule } from '../shared/shared.module';
 import { FinanceRoutingModule } from './finance-routing.module';
 
 import { RegisterDonationComponent } from './donations/register-donation/register-donation.component';
+import { DonationFormComponent } from './forms/donation-form/donation-form.component';
 
 
 @NgModule({
   declarations: [
-    RegisterDonationComponent
+    RegisterDonationComponent,
+    DonationFormComponent
   ],
   imports: [
     CommonModule,
