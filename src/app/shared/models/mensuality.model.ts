1d7bebf6de563a980ec5fd1304dddc9c92e538cf
nicole <nikybg96@gmail.com>
2019-08-25 17:47:42 -0400

    mensuality model

---
+++ src/app/shared/models/mensuality.model.ts
@@ -0,0 +1,19 @@
+export class MensualityModel {
+    $key: string;
+    firstName: string;
+    lastName: string;
+    mothersLastName: string;
+    date: Date;
+    monthToPay: string;
+    amount: string;
+
+    constructor() {
+        this.firstName = '';
+        this.firstName = '';
+        this.lastName = '';
+        this.mothersLastName = '';
+        this.date = null;
+        this.monthToPay = '';
+        this.amount = '';
+    }
+}
