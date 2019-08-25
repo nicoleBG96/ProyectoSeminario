6350c8ec8c7f72988aacf4f0ce097d653eb08860
nicole <nikybg96@gmail.com>
2019-08-25 17:54:50 -0400

    mensuality service

---
+++ src/app/shared/services/mensuality.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import {AngularFireDatabase} from 'angularfire2/database';
+import {AngularFireList} from 'angularfire2/database';
+import { AngularFireStorage } from 'angularfire2/storage';
+
+import * as fb from 'firebase';
+
+import { MensualityModel } from '../models/mensuality.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MensualityService {
+  mensualityList: AngularFireList<any>;
+
+  constructor(private firebase: AngularFireDatabase, private storage: AngularFireStorage) { }
+
+  getMensuality() {
+    return this.firebase.list('mensualities').snapshotChanges().pipe(
+      map(action => action.map(data => {
+        return {
+          key: data.payload.key,
+          ...data.payload.val()
+        };
+      })));
+  }
+
+  createMensuality(mensuality: MensualityModel) {
+    this.firebase.list('mensualities').push(mensuality);
+  }
+}
