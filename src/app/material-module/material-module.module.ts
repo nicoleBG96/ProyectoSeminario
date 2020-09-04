7226e2791b05aea7e726ba4d18949930859b5d13
n.bg96 <nikybg96@gmail.com>
2020-09-04 10:54:56 -0400

    fixes

--- src/app/material-module/material-module.module.ts
+++ src/app/material-module/material-module.module.ts
@@ -14,6 +14,7 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatDialogModule} from '@angular/material/dialog';
 
+
 @NgModule({
   declarations: [],
   imports: [
